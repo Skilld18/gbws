433ac77b57ad0c521486b8a7356ad6ddf196fe6a
russelldunk@gmail.com <Skilld2467134>
2021-10-11 15:00:56 -0400

    getting game data from stock game

--- dota2/data.py
+++ dota2/data.py
@@ -1,2 +1,10 @@
-def get_game():
-    return "rubick"
+import requests
+
+
+def get_game_data():
+    api_url = "https://api.opendota.com/api/"
+    return str(requests.get(api_url + "matches/1").content)
+
+
+def get_key():
+    return open("key", "r").read()
--- dota2/test_data.py
+++ dota2/test_data.py
@@ -3,8 +3,17 @@ import data
 
 
 class MyTestCase(unittest.TestCase):
-    def test_get_game(self):
-        self.assertNotEqual(data.get_game(), None)
+    def test_get_game_data(self):
+        self.assertNotEqual(data.get_game_data(), None)
+
+    def test_sane_get_game_data(self):
+        self.assertIn("dire", data.get_game_data())
+
+    def test_get_key(self):
+        self.assertTrue(len(data.get_key()) == 36)
+
+    def test_get_sane_key(self):
+        self.assertTrue(len(set(data.get_key())) > 1)
 
 
 if __name__ == '__main__':
