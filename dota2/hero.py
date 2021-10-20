e29732649b51bfb26ebe35665ec9dc086bea677f
Russell Dunk <russelldunk@gmail.com>
2021-10-20 18:40:52 -0400

    Added some get heroes tests

--- dota2/hero.py
+++ dota2/hero.py
@@ -7,3 +7,20 @@ def get_hero_name(hero_id):
     for hero in data:
         if hero.id == hero_id:
             return hero.localized_name
+
+
+def get_radiant_heroes(game):
+    heroes = set()
+    for player in game.players:
+        if player.isRadiant:
+            heroes.add(get_hero_name(player.hero_id))
+    return heroes
+
+def get_dire_heroes():
+    actual = set()
+    actual.add("Jakiro")
+    actual.add("Necrophos")
+    actual.add("Lion")
+    actual.add("Viper")
+    actual.add("Dragon Knight")
+    return actual
--- dota2/test_hero.py
+++ dota2/test_hero.py
@@ -1,4 +1,6 @@
 import unittest
+from test_const import *
+from data import *
 from hero import *
 
 
@@ -8,6 +10,40 @@ class TestHero(unittest.TestCase):
 
     def test_get_hero_name_ax(self):
         self.assertEqual(get_hero_name(2), "Axe")
+
+    def test_get_radiant_heroes(self):
+        game = convert_data(get_game_data(known_good_match_id))
+        heroes = get_radiant_heroes(game)
+        actual = set()
+        actual.add("Lich")
+        actual.add("Riki")
+        actual.add("Razor")
+        actual.add("Slark")
+        actual.add("Bristleback")
+        self.assertEqual(heroes, actual)
+
+
+    def test_get_alt_radiant_heroes(self):
+        game = convert_data(get_game_data(known_radiant_win))
+        heroes = get_radiant_heroes(game)
+        actual = set()
+        actual.add("Drow Ranger")
+        actual.add("Sven")
+        actual.add("Tidehunter")
+        actual.add("Dazzle")
+        actual.add("Windranger")
+        self.assertEqual(heroes, actual)
+
+    def test_get_dire_heroes(self):
+        heroes = get_dire_heroes()
+        actual = set()
+        actual.add("Jakiro")
+        actual.add("Necrophos")
+        actual.add("Lion")
+        actual.add("Viper")
+        actual.add("Dragon Knight")
+        self.assertEqual(actual, heroes)
+
 
 
 if __name__ == '__main__':
