5d4dd83ee20ae8cd32e388858e9192744f60a375
Russell Dunk <russelldunk@gmail.com>
2022-04-10 15:48:22 -0400

    creating pareto class

---
+++ pareto/situation.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Situation:
+
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def dominated(self, other):
+        pass
